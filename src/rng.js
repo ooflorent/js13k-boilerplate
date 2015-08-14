00501716a96db4902b3bdedf6622197195c7a2fb
Florent Cailhol <florent.cailhol@gmail.com>
2015-08-14 11:53:23 +0200

    Shorter random generator

--- src/rng.js
+++ src/rng.js
@@ -1,5 +1,9 @@
 module.exports = function(seed) {
-  var random = whrandom(seed);
+  function random() {
+    var x = Math.sin(.8765111159592828 + seed++) * 1e4
+    return x - Math.floor(x)
+  }
+  
   var rng = {
     /**
      * Return an integer within [0, max).
@@ -54,31 +58,3 @@ module.exports = function(seed) {
 
   return rng;
 };
-
-/**
- * Generate a seeded random number using Python's whrandom implementation.
- * See https://github.com/ianb/whrandom for more information.
- *
- * @param  {int} [seed]
- * @return {Function}
- * @api private
- */
-function whrandom(seed) {
-  if (!seed) {
-    seed = Date.now();
-  }
-
-  var x = (seed % 30268) + 1;
-  seed = (seed - (seed % 30268)) / 30268;
-  var y = (seed % 30306) + 1;
-  seed = (seed - (seed % 30306)) / 30306;
-  var z = (seed % 30322) + 1;
-  seed = (seed - (seed % 30322)) / 30322;
-
-  return function() {
-    x = (171 * x) % 30269;
-    y = (172 * y) % 30307;
-    z = (170 * z) % 30323;
-    return (x / 30269.0 + y / 30307.0 + z / 30323.0) % 1.0;
-  };
-}
